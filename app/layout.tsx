60d0715db154a93c6513e731fc2b363f4eb1eaa2
razlevio <razlevio.55@gmail.com>
2024-02-01 19:28:45 +0200

    chore(prettier): configure prettier and fixing layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,67 @@
 import "@/styles/globals.css"
+
 import { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Toaster } from "sonner"
+
 import { appConfig } from "@/config/app"
 import { geist, geistMono } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { ModalProvider } from "@/components/providers/modal-provider";
+import { ModalProvider } from "@/components/providers/modal-provider"
 import { ThemeProvider } from "@/components/providers/theme-provider"
-import { Toaster } from "sonner";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: {
-    default: appConfig.name,
-    template: "%s | " + appConfig.name,
-  },
-  applicationName: appConfig.name,
-  description: appConfig.description,
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  authors: [
-    {
-      name: "razlevio",
-      url: "https://github.com/razlevio",
-    },
-  ],
-  creator: "razlevio",
-  icons: {
-    icon: "/favicon.png",
-    shortcut: "/favicon.png",
-  },
-  verification: {
-    google: 'google',
-    yandex: 'yandex',
-  },
+	title: {
+		default: appConfig.name,
+		template: "%s | " + appConfig.name,
+	},
+	applicationName: appConfig.name,
+	description: appConfig.description,
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	authors: [
+		{
+			name: "razlevio",
+			url: "https://github.com/razlevio",
+		},
+	],
+	creator: "razlevio",
+	icons: {
+		icon: "/favicon.png",
+		shortcut: "/favicon.png",
+	},
+	verification: {
+		google: "google",
+		yandex: "yandex",
+	},
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
 		<ClerkProvider>
-			<html lang="en" className={cn("h-full", geist.className, geist.variable, geistMono.variable)} suppressHydrationWarning>
+			<html
+				lang="en"
+				className={cn("h-full", geist.className, geist.variable, geistMono.variable)}
+				suppressHydrationWarning
+			>
 				<body>
-					<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
 						<Toaster position="bottom-center" />
 						<ModalProvider />
 						{children}
@@ -64,5 +71,5 @@ export default function RootLayout({
 				</body>
 			</html>
 		</ClerkProvider>
-  )
+	)
 }
